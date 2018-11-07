53a6815a2e288f651d3c7f81834cf58b93783047
hartj430 <hartj430@gmail.com>
2018-11-07 11:48:45 -0500

    change

--- map.js
+++ map.js
@@ -6,7 +6,7 @@ mapboxgl.accessToken = 'pk.eyJ1IjoiaGFydGo0MzAiLCJhIjoiY2puZjc3c3BrMGJuYjN3bmJnZ
 
 let map = new mapboxgl.Map({
     container: 'map',
-    style: 'mapbox://styles/hartj430/cjo611lgy09ri2qpmuyp7h0or',
+    style: 'mapbox://styles/hartj430/cjo7ebb0l09fq2sldwznwmetn',
     center: [-73.96216,40.80779],
     zoom: 16
 })
@@ -49,18 +49,18 @@ geolocate.on('geolocate', function(event) {
 })
 
 // for testing purposes, also update the variable whenever you click on the map
-map.on('click', function(event) {
-   current_location = [event.lngLat.lng, event.lngLat.lat]
-   console.log('clicked', current_location)     
+//map.on('click', function(event) {
+  // current_location = [event.lngLat.lng, event.lngLat.lat]
+  // console.log('clicked', current_location)     
 
-     if (active) {                           // if we're drawing
-        path.push(current_location)         // add the current location to the path
-       console.log(path)                   // ...and for testing purposes, log the path so far to the console.
-   map.getSource('drawing').setData(geojson) 
+  //   if (active) {                           // if we're drawing
+   //     path.push(current_location)         // add the current location to the path
+  //     console.log(path)                   // ...and for testing purposes, log the path so far to the console.
+ //  map.getSource('drawing').setData(geojson) 
 
-    }
+  //  }
 
- })
+// })
 
 map.on('load', function() {             // 'load' event handler
     map.addLayer({                      // add a layer
